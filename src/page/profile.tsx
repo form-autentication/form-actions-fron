7f20c8b0ce9e53a6d3718d5c87bbd30c1ac621a1
Wdavid Calsin <david2c142@gmail.com>
2020-12-30 23:44:35 -0500

    feat: 🎸 add table

--- src/page/profile.tsx
+++ src/page/profile.tsx
@@ -29,16 +29,40 @@ const Profile = () => {
    }, []);
 
    return (
-      <div>
-         <h1>Profile</h1>
-         {dataUsers.map((res, i) => (
+      <div className="profile">
+         <div className="container">
+            <h1>Profile</h1>
+            {/* {dataUsers.map((res, i) => (
             <div key={i}>
                <h3>{res.name}</h3>
                <p>{res.email}</p>
                <p>{res.password}</p>
                <br />
             </div>
-         ))}
+         ))} */}
+
+            <table className="table">
+               <thead>
+                  <tr>
+                     <th>#</th>
+                     <th>Nombres</th>
+                     <th>Email</th>
+                     <th>Password</th>
+                     {/* <th></th> */}
+                  </tr>
+               </thead>
+               <tbody>
+                  {dataUsers.map((res, i) => (
+                     <tr ng-repeat="item in lista" key={i}>
+                        <td>{i + 1}</td>
+                        <td>{res.name}</td>
+                        <td>{res.email}</td>
+                        <td>{res.password}</td>
+                     </tr>
+                  ))}
+               </tbody>
+            </table>
+         </div>
       </div>
    );
 };
